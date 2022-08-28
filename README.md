03a5ba05a42519ff80a1193fc3b40b5bb787c889
fengwei2002 <konng0120@gmail.com>
2022-08-28 11:30:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-gmj2qi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-gmj2qi)
---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <MonacoEditor id="editor" />
+</template>
+
+<style>
+html,
+body,
+#__nuxt,
+#editor {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
