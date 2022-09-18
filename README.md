b0cd534a3cce57c11ac5579575dce462eb5899a9
fengwei2002 <konng0120@gmail.com>
2022-09-18 13:10:10 +0800

    use origin monaco and KONNG theme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-## pure editor
+## pure editoruse
+
+- use https://github.com/fengwei2002/colorSchemeTool to generate .tmTheme file
+- use https://bitwiser.in/monaco-themes/ to generate manaco theme js part 
--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineNuxtConfig } from "nuxt";
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-    modules: ["nuxt-monaco-editor"],
-    monacoEditor: { lang: "zh-cn" },
-});
